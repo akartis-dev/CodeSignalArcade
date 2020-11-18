b767496c69d085b7426a9091bc3533e379a678ea
akartis-dev <akartis-dev@sitraka.com>
2020-11-18 08:47:14 +0300

    isBeautifulString fail in hidden test 18/20

---
+++ Eruption_of_Light/isBeautifulString.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Eruption of Light
+ * isBeautifulString
+ * Pass 18/20 
+ * Fail in hidden test
+ */
+
+function isBeautifulString($inputString) {
+    $inputString = str_split($inputString);
+    $word = array_count_values($inputString);
+    ksort($word);
+    print_r($word);
+    $char = array_values(range('a', 'z'));
+
+
+    $f_key = array_key_first($word);
+    $max = $word[$f_key];
+
+    foreach($word as $k => $c){
+
+        $alpha_index = array_search($k, $char);
+        $l_key = array_key_last($word);
+        $l_index = array_search($l_key, $char);
+
+        if($alpha_index !== false && $l_index !== false){
+            if( $alpha_index === 0 &&
+                !array_key_exists($char[$alpha_index + 1], $word)
+            ){
+                echo "1";
+                return false;
+            }
+
+            if( $alpha_index > 0 &&
+                !array_key_exists($char[$alpha_index - 1], $word)
+                ){
+                    echo "2" . $alpha_index;
+                    return false;
+            }
+
+            if($alpha_index < $l_index &&
+              !array_key_exists($char[$alpha_index + 1], $word)
+            ){
+                echo "3" . $alpha_index;
+                return false;
+            }
+        }
+       
+        if($c > $max){
+            return false;
+        }
+    }
+    return true;
+}
+
+var_dump(isBeautifulString("bbbaacdafe"));
